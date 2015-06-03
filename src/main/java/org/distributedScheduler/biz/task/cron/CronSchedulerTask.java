8cfe44089c5167714cf165ddbb09b9565f4608d9
chenk008 <kongchen28@gmail.com>
2015-06-03 16:03:49 +0800

    增加quartz的cron

--- src/main/java/org/distributedScheduler/biz/task/cron/CronSchedulerTask.java
+++ src/main/java/org/distributedScheduler/biz/task/cron/CronSchedulerTask.java
@@ -1,12 +1,25 @@
 package org.distributedScheduler.biz.task.cron;
 
+import java.util.Map;
+
 import org.distributedScheduler.biz.task.Task;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
 
-public abstract class CronSchedulerTask implements Task {
+public abstract class CronSchedulerTask implements Task, Job {
 
 	private static SchedulerFactory schedulerFactory = new StdSchedulerFactory();
 
@@ -15,11 +28,33 @@ public abstract class CronSchedulerTask implements Task {
 		Scheduler scheduler;
 		try {
 			scheduler = schedulerFactory.getScheduler();
+			JobDetail job = JobBuilder.newJob(this.getClass())
+					.withIdentity(this.getClass().getName()).build();
 
+			DateTimeFormatter df = DateTimeFormat
+					.forPattern("yyyy-MM-dd HH:mm:ss");
+			Trigger trigger = TriggerBuilder
+					.newTrigger()
+					.withIdentity(
+							new TriggerKey(this.getClass().getName(),
+									"CronSchedulerTask"))
+					.withSchedule(
+							CronScheduleBuilder
+									.cronSchedule(getCronExpression()))
+					.startAt(df.parseDateTime(getStartTime()).toDate()).build();
+
+			scheduler.scheduleJob(job, trigger);
+			scheduler.start();
 		} catch (SchedulerException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		getData();
 	}
 
 	@Override
@@ -34,4 +69,8 @@ public abstract class CronSchedulerTask implements Task {
 	}
 
 	public abstract String getCronExpression();
+
+	public abstract String getStartTime();
+
+	public abstract Map<String, Object> getData();
 }
